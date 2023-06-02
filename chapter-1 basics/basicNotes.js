1e558f75b71e00f202c5049264437406b6869653
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-02 20:38:08 +0530

    added basic notes for js

---
+++ chapter-1 basics/basicNotes.js
@@ -0,0 +1,64 @@
+// // // basic javascript
+
+
+// // // to print anything
+// // console.log(5+20);
+// // console.log('hello world');
+
+
+
+// // // variables
+// // var myAge = 19;
+// // let myName = 'Akshita';
+// // const mySurname = 'Gupta';
+// // // let can be redefined in the code later whereas const cannot
+
+
+
+// // // data types
+// // // undefined : typeof instance === "undefined"
+// // // Boolean : typeof instance === "boolean"
+// // // Number : typeof instance === "number"
+// // // String : typeof instance === "string"
+// // // BigInt : typeof instance === "bigint"
+// // // Symbol : typeof instance === "symbol"
+
+
+
+// // // typeof operator
+// // // console.log(typeof(variableName));
+// // console.log(typeof(myName));
+
+
+
+// // // difference between null and undefined
+
+// // var variableName = null;
+// // console.log(variableName); // prints null
+// // // null value is assigned to the variable
+
+// // var iAmStandBy;
+// // console.log(iAmStandBy); // prints undefined
+// // // no value is assigned to the variable, hence it is undefined
+
+// // // data type of null is object
+// // // data type of undefined is undefined 
+// // // this is a bug in javascript
+
+
+
+// // // what is NaN (not a number)
+// // console.log(isNaN(myName));
+// // console.log(isNaN(myName)); // returns true
+// // if(isNaN(myName)){
+// //     console.log('please enter valid phone number');
+// // }
+
+
+
+// // // NaN is a property of a global object, its not a data type
+// // NaN === NaN; //returns false
+// // Number.NaN === NaN; // returns false
+// // isNaN(NaN); // returns true
+// // isNaN(Number.NaN); // returns true
+// // Number.isNaN(NaN); // true
