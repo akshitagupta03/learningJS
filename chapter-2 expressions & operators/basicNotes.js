45d1a5a35acb4b221b0af2b0b11aa9069cd0adb9
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-02 20:39:15 +0530

    notes for expressions and operators

---
+++ chapter-2 expressions & operators/basicNotes.js
@@ -0,0 +1,47 @@
+// // // expressions and operators
+
+// // 5 + 20; // 5 and 20 are the operands, + is the operator
+// // // this whole thing is an expression
+
+// // // assignment operator
+// // var x = 5;
+
+// // // arithmetic operator
+// // console.log(3+3);
+// // console.log(3**3); // 3 to the power 3, returns 27
+
+// // // increment decrement operators
+
+// // // postfix operator
+// // var num = 15;
+// // var newNum = num++;
+// // console.log(num); // prints 16
+// // console.log(newNum); // prints 15
+
+// // // prefix operator
+// // var num = 15;
+// // var newNum = ++num;
+// // console.log(num); // prints 16
+// // console.log(newNum); // prints 16
+
+// // // comparison operator
+// // var a = 30;
+// // var b = 10;
+// // console.log(a == b); // returns false 
+
+// // // logical operator
+
+// // // logical  AND (&&)
+// // // logical OR (||)
+// // // logical NOT (!)
+
+// // // string operator
+
+// // // concatenation operator
+// // console.log("hello " + "world"); // returns hello world
+// // console.log(myName + " Gupta"); 
+
+// // // conditional (ternary) operator
+// // // variableName = (condition) ? value1 : value2
+// // var age = 17;
+// // console.log((age >= 18) ? "you can vote" : "you cannot vote");
