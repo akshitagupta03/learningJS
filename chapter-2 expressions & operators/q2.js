01e8c2f530e65ae48f128832533678103ddd1808
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-02 20:40:31 +0530

    basic questions

---
+++ chapter-2 expressions & operators/q2.js
@@ -0,0 +1,8 @@
+// // // swap two numbers without using third variable
+// // var a = 5;
+// // var b = 10;
+// // a = a+b; // a = 15
+// // b = a-b; // b = 5
+// // a = a-b; // a = 10
+// // console.log("The value of a is " + a);
+// // console.log("The value of b is " + b);
