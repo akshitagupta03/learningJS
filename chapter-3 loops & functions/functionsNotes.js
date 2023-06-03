52b6021193a0e596bde0b1f1208087fbeb4dc0e5
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-03 15:58:35 +0530

    some notes for functions

---
+++ chapter-3 loops & functions/functionsNotes.js
@@ -0,0 +1,18 @@
+// // function sum(){
+// //     var a=10, b=20;
+// //     var total = a+b;
+// //     console.log(total);
+// // }
+// // sum();
+
+// // // anonymous function
+// // // putting a function into a variable
+// // var funExp = function(a, b){
+// //     return total=a+b;
+// // }
+// // console.log(funExp(5, 15));
+
+
+// // // var vs let vs const
+// // // var => function scope
+// // // let and const => block scope
