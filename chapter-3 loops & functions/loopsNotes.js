1b26ceac4d118132c78fc9bc3f9e0f34156c1426
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-03 15:58:02 +0530

    added some notes for loops

---
+++ chapter-3 loops & functions/loopsNotes.js
@@ -0,0 +1,22 @@
+// // // if-else
+// // var tomr = 'rain';
+// // if(tomr == 'rain'){
+// //     console.log('Take a raincoat');
+// // }
+// // else{
+// //     console.log('No need to take a raincoat');
+// // }
+
+
+
+// // // what are falsy values
+// // // there are 5 falsy values in javascript
+// // // 0, "", undefined, null, NaN, false
+
+// // if(score=0){
+// //     console.log("Omg, we lost the game");
+// // }
+// // else{
+// //     console.log("Yay, we won the game");
+// // }
+// // // always returns yay, we won the game as score=0 is a falsy value
