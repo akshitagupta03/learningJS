aaaaa3eae0535dc72db4eb3e19b9cce22e2f1864
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-04 00:12:45 +0530

    notes for arrays in javascript

---
+++ chapter-4 arrays/notes.js
@@ -0,0 +1,15 @@
+// // var myFriends = ["megha", "parth", "kanishka"];
+// // console.log(myFriends[2]); // returns kanishka
+
+// // for(let elements of myFriends){
+// //     console.log(elements);
+// // }
+
+// // myFriends.forEach(function (element, index, array){
+// //     console.log(element + " index : " + index + " " + array);
+// // })
+
+// // // fat arrow function
+// // myFriends.forEach((element, index, array) => {
+// //     console.log(element + " index : " + index + " " + array);
+// // })
