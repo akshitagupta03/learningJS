d94499f8b91fb0a7697c61ca0205ed2001ed9f00
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-04 00:16:33 +0530

    notes for strings in js

---
+++ chapter-4 arrays/stringsNotes.js
@@ -0,0 +1,19 @@
+// strings can be created as primitives, from string literals, or as objects using the String() constructor
+// let myName = "akshita gupta";
+// let myChannelName = 'akshita gupta';
+
+// // do not use this
+// // let ytName = new String("Thapa Technical");
+
+// console.log(myName);
+// console.log((ytName));
+
+
+
+// // escape character
+// // to use if we want quotes on a word in a string as well
+// let anySentence = "we are the so called \"Vikings\" from the north.";
+// console.log(anySentence);
+
+// // if you don't want any mess, simply use alternate quotes
+// let anySentence = "we are the so called 'Vikings' from the north.";
