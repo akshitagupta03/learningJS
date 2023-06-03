8bc157ebd57a0abbd30aa67db69f9a2c7ea39291
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-03 15:56:49 +0530

    some more notes

---
+++ chapter-2 expressions & operators/difference.js
@@ -0,0 +1,6 @@
+// // // difference between  == and ===
+// // var num1 = 5;
+// // var num2 = '5';
+
+// // console.log(num1 == num2); // returns true
+// // console.log(num1 === num2); // returns false
