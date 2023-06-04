e7738c45bac1fb7d885eb400e3b3cb125dfd59ea
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-05 00:54:58 +0530

    methods in dates

---
+++ chapter-5 date and time/datesMethods.js
@@ -0,0 +1,22 @@
+// const curDate = new Date();
+
+/* 
+how to get individual date
+console.log(curDate.toLocaleString());
+console.log(curDate.getFullYear());
+console.log(curDate.getMonth()); // 0-11 jan to dec
+console.log(curDate.getDate());
+console.log(curDate.getDay()); 
+*/
+
+// const curDate = new Date();
+
+/* 
+how to set individual date
+console.log(curDate.setFullYear(2023));
+the setFullYear() method can optionally set month and day
+console.log(curDate.setFullYear(2023, 5, 1));
+console.log(curDate.setMonth(5));
+console.log(curDate.setDate(1));
+console.log(curDate.toLocaleString()); 
+*/
