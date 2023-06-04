096cd105e0f23f496a33899ef922381579a15b7b
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-05 01:15:49 +0530

    notes for time in js

---
+++ chapter-5 date and time/timeNotes.js
@@ -0,0 +1,35 @@
+// const curTime = new Date();
+
+/* 
+how to get individul time
+console.log(curTime.getTime());
+getTime() returns number of milliseconds since January 1, 1970
+console.log(curTime.getHours());
+returns the hours of a date as a number(0-23)
+console.log(curTime.getMinutes());
+console.log(curTime.getSeconds());
+console.log(curTime.getMilliseconds()); 
+*/
+
+/* 
+how to set individual time
+let curTime = new Date();
+
+// console.log(curTime.setTime());
+console.log(curTime.setHours(20));
+console.log(curTime.setMinutes(56));
+console.log(curTime.setSeconds(5));
+console.log(curTime.setMilliseconds(231)); 
+*/
+
+
+/* 
+// to get time
+new Date().toLocaleTimeString();
+
+// to get date
+new Date().toLocaleDateString();
+
+// to get both
+new Date().toLocaleString();
+*/
