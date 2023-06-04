7696e38346747bced1a215b61f0ba9eaf4669bf9
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-05 01:16:22 +0530

    math objects in javascript

---
+++ chapter-5 date and time/mathObjects.js
@@ -0,0 +1,33 @@
+// math objects in javascript
+// allows to perform mathematical tasks on numbers
+
+// console.log(Math.PI);
+
+/* 
+Math.round()
+returns value of x rounded to its nearest integer
+let num = 10.2565;
+console.log(Math.round(num));
+*/
+
+/* 
+Math.pow()
+console.log(Math.pow(2, 3));
+
+Math.sqrt()
+console.log(Math.sqrt(25));
+
+Math.abs()
+console.log(Math.abs(-55));
+
+Math.ceil()
+Math.floor()
+Math.min()
+Math.max()
+
+Math.random()
+returns a random number between 0 (inclusive), and 1 (exclusive)
+
+Math.trunc()
+returns integer part of a number 
+*/
