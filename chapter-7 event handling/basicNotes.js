e637bbee315ac965abcc8d26e3efb5e1b33adcb5
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-07 00:55:38 +0530

    basic notes for events

---
+++ chapter-7 event handling/basicNotes.js
@@ -0,0 +1,42 @@
+// HTML events are things that happen to HTML elements
+// when JS is used in HTML pages, it can "react" on these events
+// an HTML event can be something the browser or user does
+
+
+
+/* 
+
+4 ways of writing events in JavaScript
+
+1. using inline events alert();
+2. by calling a function (most common way or writing)
+3. using inline events (HTML onclick="" property and element.onclick)
+4. using Event Listeners (aaddEventListener and IE's attachEvent) 
+
+*/
+
+
+
+/* 
+
+what is Event object?
+Event object is the parent object of the event object
+for eg: mouseEvent, focusEvent, KeyboardEvent 
+
+
+// mouseEvent
+events that occur when mouse interacts with the document
+
+
+// keyboardEvent
+events that occur when user presses a key on the keyboard
+belongs to the KeyboardEvent Object
+onkeypress, onkeydown, onkeyup
+
+
+// inputEvents
+the onchange event occurs when the value of an element has been changed
+for radio buttons and checkboxes, the onchange event occurs
+when the checked state has been changed 
+
+*/
