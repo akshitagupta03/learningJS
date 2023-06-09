2029aed76590026180d2b1f164b429ae64a65c53
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-10 02:26:59 +0530

    concept of timing in js

---
+++ chapter-8 timing/basicNotes.js
@@ -0,0 +1,16 @@
+/*
+
+Javascript timing events
+
+the window object allows execution of code at specified time intervals
+these time intervals are called timing events
+
+two key methods are:
+
+setTimeout(function, milliseconds)
+// executes a function after waiting a specified number of ms
+
+setInterval(function, milliseconds)
+// same as setTimeout() but repeats the execution of function continuously
+
+*/
